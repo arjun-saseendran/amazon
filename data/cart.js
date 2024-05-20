1c721f7e7e8b60eddf8a05367dd8a7145c37804a
arjunsaseendran91 <arjunsaseendran@icloud.com>
2024-05-20 11:50:33 +0530

    add storage & add to cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [{
+
+
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
 productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
 quantity: 2,
 },
@@ -7,6 +9,10 @@ quantity: 2,
   quantity: 1,
   }
 ];
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -23,6 +29,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -34,5 +41,5 @@ if(cartItem.productId !==  productId){
 }
   })
   cart = newCart;
-
+saveToStorage();
 }
