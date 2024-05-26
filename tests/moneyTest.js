371d985d7fbe5ab114d5909a3e12a61722d47849
arjunsaseendran91 <arjunsaseendran@icloud.com>
2024-05-26 19:50:10 +0530

    remove default test and add moneyTest

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -6,14 +6,14 @@ console.log('Convert cents in to dollars');
 if(formatCurrency(2095) === '20.95'){
   console.log('Passed');
 }else{
-  console.log('Faild');
+  console.log('Failed');
 }
 
 console.log('Work with 0');
 if(formatCurrency(0) === '0.00'){
   console.log('Passed');
 }else{
-  console.log('Faild');
+  console.log('Failed');
 }
 
 
